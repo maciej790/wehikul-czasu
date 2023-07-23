b749eea3a69086d664af4a96bcf5c9b3c83d3a6b
maciej790 <maciejmw126@gmail.com>
2023-07-23 14:32:44 +0200

    change cms version

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,5 +1,4 @@
-import CMS from 'netlify-cms-app'
-// Initialize the CMS object
-CMS.init()
+import CMS from 'netlify-cms'
+
 // Now the registry is available via the CMS object.
 CMS.registerPreviewTemplate('my-template', MyTemplate)
