cfa759c88c8494767b53804112287517e22c0221
maciej790 <maciejmw126@gmail.com>
2023-07-23 17:06:02 +0200

    aaaaccaaav3

--- src/admin/index.js
+++ src/admin/index.js
@@ -2,4 +2,4 @@ import CMS from 'netlify-cms'
 
 import { UuidControl, UuidPreview } from 'netlify-cms-widget-uuid-v4'
 
-CMS.registerWidget('uuid', UuidControl)
+CMS.registerWidget('uuid', UuidControl, UuidPreview)
