d4acae7b0546f3eecf248c50d3571d52b5fa1d0c
maciej790 <maciejmw126@gmail.com>
2023-08-04 22:03:34 +0200

    relations

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,42 +1,11 @@
-// This is where project configuration and plugin options are located.
+// This is where project configuration and plugin options are located. 
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: [
-    { 
-      use: '@gridsome/source-filesystem',
-      options: { 
-        path: 'content/categories/**/*.md',
-        typeName: 'Category',
-      } 
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'content/posts/**/*.md',
-        typeName: 'Post',
-        route: '/post/:year/:month/:day/:slug',
-        refs: {
-            category: 'Category', 
-        },
-        remark: {
-          //Config options can be added here
-        }
-      }
-    },
-    {
-      use: `gridsome-plugin-netlify-cms`,
-      options: {
-        publicPath: `/admin`,
-        modulePath: `src/admin/index.js`
-      }
-    },
-  ],
-
+  siteName: 'Gridsome + NetlifyCMS - Relations',
   transformers: {
     remark: {
       externalLinksTarget: '_blank',
@@ -46,5 +15,33 @@ module.exports = {
         // ...global plugins
       ]
     }
-  }
+  },
+
+  plugins: [
+    { 
+      use: '@gridsome/source-filesystem',
+      options: { 
+        path: 'content/authors/*.md',
+        typeName: 'Author',
+      } 
+    },
+    { 
+      use: '@gridsome/source-filesystem',
+      options: { 
+        path: 'content/blog/*.md',
+        typeName: 'BlogPost',
+        route: '/blog/:year/:month/:day/:slug',
+        refs: { 
+          author: 'Author', 
+        } 
+      } 
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`,
+        modulePath: `src/admin/index.js` // This is important and allows us to register our own widget
+      }
+    },
+  ]
 }
