e88cc13737755f581f46bb4364b690233f6d84b4
maciej790 <maciejmw126@gmail.com>
2023-08-13 17:12:11 +0200

    cms relations

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,20 +1,3 @@
-// src/js/cms.js or wherever you set this file up
-import CMS from "netlify-cms-app";
+import CMS from 'netlify-cms'
 
-import {Widget as RelationWidget} from "@ncwidgets/file-relation";
-import {Widget as IdWidget} from "@ncwidgets/id";
-
-// use 'ncw-file-relation' in config.yml
-CMS.registerWidget(RelationWidget);
-
-// use 'ncw-id' in config.yml
-CMS.registerWidget(IdWidget);
-
-/* If you prefer to use your own widget name in config.yml, do this
- * CMS.registerWidget({
- *   name: 'my-custom-name',
- *   ...RelationWidget, 
- * })
- */
-
-CMS.init();
+CMS.init()
