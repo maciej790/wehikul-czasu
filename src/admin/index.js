f5ef89a645a16060c166c43f32d97be95e0a9d61
maciej790 <maciejmw126@gmail.com>
2023-08-13 18:05:10 +0200

    new relations cms v5

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,1 +1,6 @@
-import CMS from 'netlify-cms'
+// Register the widget
+import cms from 'netlify-cms-app'
+import { Widget as IdWidget } from '@ncwidgets/id'
+
+cms.registerWidget(IdWidget)
+cms.init()
