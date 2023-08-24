57163c3df35bcd7fd789aadbb385171edb0171ba
maciej790 <maciejmw126@gmail.com>
2023-08-24 12:40:41 +0200

    uuid v2

--- src/admin/index.js
+++ src/admin/index.js
@@ -31,4 +31,4 @@ const IdPreview = window.createClass({
 });
 
 // Register the widget. This lets NetlifyCMS know about our custom widget
-//CMS.registerWidget('id', IdControl, IdPreview);
+CMS.registerWidget('id', IdControl, IdPreview);
